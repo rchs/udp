4328e5e36289c48778188d844a04618c8d0eaf40
Ranjan Shrestha <ranjan@sharingapples.com>
2019-10-01 15:45:10 +0545

    Change UDP to reliable UDP

--- src/UDPSocket.ts
+++ src/UDPSocket.ts
@@ -1,95 +1,128 @@
 import { Socket, RemoteInfo } from 'dgram';
-import { strToUint8, uint8ToStr } from './strToUint8';
-import powmod from './powmod'
+import { RxBuffer, TxQueue, parsePayload, createPayload } from './utils';
+
+/**
+ * Packet format
+ * Byte 0: Version
+ * Byte 1: packet Type
+ *    0: Broadcast
+ *    1: Open
+ *    2: Payload
+ *    3: Close
+ * Byte 2: Seq Number
+ * Byte 3: Ack Number
+ * Byte 4: Chunk Number
+ */
 
 type AddressInfo = {
   address: string,
   port: number,
 }
 
-const HEADER = strToUint8('["",');
+const VERSION = 1;
+const MAX_TRIES = 3;
 
-const CONNECT = 1;
-const BCAST = 0;
-const DATA = 2;
-const CHUNK_SIZE = 1000;
+const STATE_NONE = 0;
+const STATE_LISTENING = 1;
+const STATE_CONNECTING = 2;
+const STATE_CONNECTED = 3;
+const STATE_CLOSING = 4;
+type State = 0 | 1 | 2 | 3 | 4;
 
-const SERVER_SIDE_RETRIES = 20;
-const CLIENT_SIDE_RETRIES = 5;
+const MSG_BCAST = 0x00;
+const MSG_DATA = 0x80;
+const MSG_CONNECT = MSG_DATA | 0x01;
+const MSG_MESSAGE = MSG_DATA | 0x02;
+const MSG_CLOSE = MSG_DATA | 0x04;
 
-const MODE_CLIENT = 1;
-const MODE_SERVER = 2;
-const MODE_NONE = 0;
-type Mode = 0 | 1 | 2;
+const CODE_NO_RESPONSE = 1;
+const CODE_NORMAL = 0;
 
-const DIFFE_BASE = 46021;
-const DIFFE_MODULO = 46147;
-const ONE_BILLION = 1000000000;
-const randomKey = () => ONE_BILLION + Math.floor(Math.random() * ONE_BILLION);
-
-type ConnectionPayload = [
-  /**
-   * Public Key
-   */
-  number, // Public Key
-  /**
-   * Timestamp
-   */
-  number, //
-  /**
-   * Connection payload
-   */
-  any?
-];
+function incr(num: number) {
+  return (num + 1) % 256;
+}
 
 export class UDPSocket {
-  private socket: Socket;
-  private port: number;
+  // the underlying socket instance. It is set to null once the socket is terminated
+  private socket: Socket = null;
+
+  // the port to which the socket is bound. Until the udp socket is bound to any specific
+  // port, it is zero
+  private port: number = 0;
+
+  // The mode on which this socket is
+  private state: State = STATE_NONE;
 
   onError: (err: Error) => void;
   onReady: (socket: Socket) => void;
-  onBroadcast: (msg: any, rInfo: AddressInfo) => void;
+  onBroadcast: (msg: any, rInfo: AddressInfo, version: number) => void;
   // Return the version number
   onInit: (payload: any) => any;
   onConnection: (socket: UDPSocket, payload: any, initPayload: any) => void;
   onConnect: (initPayload: any) => void;
   onMessage: (msg: any) => void;
-  onClose: () => void;
+  onClose: (code: number) => void;
 
-  private onTerminate: () => void;
-
-  private mode: Mode = MODE_NONE;
-  private remoteAddress: AddressInfo;
-  private seq: number;
-  private ack: number;
-  private retries: number;
-  private privateKey: number;
-  private sharedKey: number;
   private clients: {[address:string]: UDPSocket};
 
-  private _try: number;
-  private _txTimer: ReturnType<typeof setTimeout>;
-  private txPending: [number, number, Uint8Array];
-  private txQueue: Array<[number, number, Uint8Array]>;
-  private rxBuffer: Uint8Array;
+  private remoteAddress: AddressInfo;
+  private version: number;
+  private seq: number;
+  private ack: number;
+  private rx: RxBuffer;
+  private tx: TxQueue;
   private timeShift: number;
+  private _onTerminate: (code: number) => void;
 
   constructor(socket: Socket, port?: number) {
     this.socket = socket;
-    this.privateKey = randomKey();
 
     if (port !== null) {
       // setup port to listen for boardcast messages as well
+      socket.on('error', (err) => {
+        if (this.onError) this.onError(err);
+      });
+
       socket.bind(port, () => {
         const address = socket.address() as AddressInfo;
-        // @ts-ignore
         this.port = address.port;
-        socket.on('message', this.handleMessage);
-        if (this.onReady) {
-          this.onReady(socket);
-        }
+        // Attach the message handler
+        socket.on('message', this._handleMessage);
+        if (this.onReady) this.onReady(socket);
       });
     }
+  }
+
+  listen() {
+    if (this.state !== STATE_NONE) throw new Error('Invalid socket mode. Cannot change a client socket to server');
+    if (!this.onConnection) throw new Error('Listening sockets must provide onConnection handler');
+    this.state = STATE_LISTENING;
+    this.clients = {};
+  }
+
+  /**
+   * Connect to the remote server with the connection payload, most likely for verification. It
+   * triggers onInit on remote server with the given payload.
+   *
+   * @param to { address, port }
+   * @param token any token that is passed as is to the server. Make sure it doesn't cross
+   *              the 1000 bytes UDP packet limit.
+   */
+  connect(to: AddressInfo, token: any) {
+    if (this.state !== STATE_NONE) throw new Error('Invalid socket mode. Cannot change a server socket to a client');
+    this._init(to, VERSION);
+    this._terminate = () => {
+      this.socket.close();
+      this.socket = null;
+    }
+    this.state = STATE_CONNECTING;
+    this.remoteAddress = to;
+    this.tx.add(MSG_CONNECT, [Date.now(), token]);
+    this.tx.try();
+  }
+
+  broadcast(message: any, address: string = '255.255.255.255') {
+    this._send(MSG_BCAST, this.port, address, createPayload(message));
   }
 
   getTimeShift(): number {
@@ -106,285 +139,166 @@ export class UDPSocket {
     return null;
   }
 
-  private _parse(data: Uint8Array) {
-    if (!this.sharedKey) return;
-    let i = 4;  // Never encrypt the header
-    const b0 = this.sharedKey & 0xff;
-    const b1 = (this.sharedKey >> 8) & 0xff;
-    for (; i < data.length - 1; i += 2) {
-      data[i] ^= b0;
-      data[i + 1] ^= b1;
-    }
-
-    if (i < data.length) data[i] ^= b0;
-  }
-
-  terminate() {
-    if (this.txPending) this._clearTransmit();
-    this.txPending = null;
-    this.rxBuffer = null;
-    this.txQueue = null;
-
-    // Clear the mode field flag
-    this.mode = MODE_NONE;
-    if (this.onTerminate) {
-      this.onTerminate();
-    } else {
-      this.socket.close();
-      this.socket = null;
-    }
-
-    if (this.onClose) this.onClose();
-  }
-
-  handleMessage = (data: Uint8Array, rinfo: RemoteInfo) => {
+  private _handleMessage = (data: Uint8Array, rInfo: RemoteInfo) => {
     const type = data[0];
-    const remoteId = `${rinfo.address}:${rinfo.port}`;
-    if (type === BCAST) {
-      if (this.onBroadcast) {
-        this.onBroadcast(this._parseBcast(data), rinfo);
-      }
-    } else if (type === CONNECT) {
-      // Only allowed in Server mode
-      if (this.mode === MODE_SERVER) {
-        const payload = this._parseConnect(data);
-        const initPayload = this.onInit ? this.onInit(payload[2]) : null;
+    const version = data[1];
 
-        // close existing connection if any
-        if (this.clients[remoteId]) {
-          this.clients[remoteId].close();
-        }
+    // Handle broadcast separately
+    if (type === MSG_BCAST) {
+      // Replace with the valid json characters
+      if (this.onBroadcast) this.onBroadcast(parsePayload(data), rInfo, version);
+      return;
+    }
 
-        const sock = new UDPSocket(this.socket, null);
-        sock.timeShift = Date.now() - payload[1];
-        sock.init(rinfo, SERVER_SIDE_RETRIES, payload[0]);
-        sock.onTerminate = () => {
+    if (this.state !== STATE_LISTENING) {
+      this._processData(data, rInfo);
+    } else {
+      const remoteId = `${rInfo.address}:${rInfo.port}`;
+      const client = this.clients[remoteId];
+      if (client) {
+        client._processData(data, rInfo);
+      } else if (type === MSG_CONNECT) {
+        const payload = parsePayload(data);
+        const connPayload = this.onInit ? this.onInit(payload) : null;
+        const newClient = new UDPSocket(this.socket, null);
+        this.clients[remoteId] = newClient;
+        newClient._init(rInfo, version);
+        newClient.seq = 1;
+        newClient._onTerminate = (code: number) => {
           delete this.clients[remoteId];
+          newClient.socket = null;
+          if (this.state === STATE_CLOSING) {
+            if (Object.keys(this.clients).length === 0) {
+              this.state = STATE_NONE;
+              this.socket.close();
+              this.socket = null;
+              this.onClose(CODE_NORMAL);
+            }
+          }
         }
-        const publicKey = powmod(DIFFE_BASE, this.privateKey, DIFFE_MODULO);
-        const buf = this._prepareConnect([publicKey, Date.now()]);
-        sock._send(buf, 0, buf.length, rinfo.port, rinfo.address);
-        this.clients[remoteId] = sock;
-        this.onConnection(sock, payload[2], initPayload);
-      } else if (this.mode === MODE_CLIENT) {
-        const payload = this._parseConnect(data);
-        this.init(rinfo, CLIENT_SIDE_RETRIES, payload[0]);
-        this.timeShift = Date.now() - payload[1];
-        if (this.onConnect) this.onConnect(payload[2]);
-      }
-    } else if (type === DATA) {
-      if (this.mode === MODE_CLIENT) {
-        this.handleData(data);
-      } else if (this.mode === MODE_SERVER) {
-        const sock = this.clients[remoteId];
-        if (sock) {
-          sock.handleData(data);
-        }
+        newClient.tx.add(MSG_CONNECT, connPayload);
+        newClient.tx.try();
+        // Try to initialize the client
+        this.onConnection(newClient, payload, connPayload);
       }
     }
   }
 
-  private init(rinfo: AddressInfo, retries: number, publicKey: number) {
-    this.mode = MODE_CLIENT;
+  private _processData(data: Uint8Array, rInfo: RemoteInfo) {
+    const type = data[0];
+    const version = data[1];
+    const ack = data[2];
+    const seq = data[3];
+
+    if (ack === this.seq) {
+      // We got ourselves an acknowledgement for the sent data
+      // Clear pending tx
+      this.seq = incr(this.seq);
+      const type = this.tx.pop();
+
+      // We got acknowledgement for our close, it's time for termination
+      if (type === MSG_CLOSE) {
+        this._terminate(CODE_NORMAL);
+      }
+    }
+
+    if (seq === incr(this.ack)) {
+      // We got some data to process
+      this.ack = seq;
+      const payload = this.rx.append(data);
+      if (payload !== undefined) {
+        if (type === MSG_DATA) {
+          this.onMessage(payload);
+          this.tx.activate(MSG_DATA);
+        } else if (type === MSG_CONNECT) {
+          this.onConnect(payload);
+          this.tx.activate(MSG_DATA);
+        } else if (type === MSG_CLOSE) {
+          this.close(payload);
+        }
+      }
+    }
+
+    this.tx.try();
+  }
+
+  private _terminate(code: number) {
+    // Make sure we release all the resources
+    this.tx.abort();
+    this.rx.clear();
+
+    if (this._onTerminate) this._onTerminate(code);
+    if (this.onClose) this.onClose(code);
+  }
+
+  private _init(rinfo: AddressInfo, version: number) {
+    this.state = STATE_CONNECTED;
     this.remoteAddress = rinfo;
     this.seq = 0;
     this.ack = 0;
-    this.sharedKey = powmod(publicKey, this.privateKey, DIFFE_MODULO);
-    this.retries = retries;
-    this.rxBuffer = null;
-    this.txQueue = [];
-    this.txPending = null;
+    this.version = version < VERSION ? version : VERSION;
+    this.rx = new RxBuffer();
+
+    this.tx = new TxQueue(() => {
+      this.tx.tries += 1;
+      if (this.tx.tries >= MAX_TRIES) {
+        return this._terminate(CODE_NO_RESPONSE);
+      }
+
+      const [type, buffer, chunk, offset, length, isEmpty] = this.tx.peek();
+      this._send(type, this.remoteAddress.port, this.remoteAddress.address, buffer, offset, length, chunk, isEmpty);
+    });
   }
 
-  close() {
-    if (this.mode === MODE_SERVER) {
-      // Close all client sockets
-      Object.values(this.clients).forEach(client => client.close());
+  close(reason: number = 0) {
+    if (this.state === STATE_CLOSING) {
+      return;
+    }
 
-      // Close the underlying udp socket
-      this.socket.close();
-    } else if (this.mode === MODE_CLIENT) {
-      this.terminate();
-    } else if (this.socket) {
-      this.socket.close();
+    if (this.state === STATE_LISTENING) {
+      this.state = STATE_CLOSING;
+      const clients = Object.values(this.clients);
+      if (clients.length === 0) {
+        this.socket.close();
+        this.socket = null;
+        if (this.onClose) this.onClose(reason);
+      } else {
+        clients.forEach(client => client.close());
+      }
+    } else {
+      this.state = STATE_CLOSING;
+      this.tx.add(MSG_CLOSE, reason);
+      this.tx.try();
     }
   }
 
   clone() {
     // Use this method to forward socket from one UDPSocket shell to another
-    if (this.mode !== MODE_NONE) throw new Error('Can only clone an uninitialized socket. Client/Server sockets cannot be cloned');
-    this.socket.removeListener('message', this.handleMessage);
+    if (this.state !== STATE_NONE) throw new Error('Can only clone an uninitialized socket. Client/Server sockets cannot be cloned');
+    this.socket.removeListener('message', this._handleMessage);
     const clone = new UDPSocket(this.socket, null);
     clone.port = this.port;
-    clone.socket.on('message', clone.handleMessage);
+    clone.socket.on('message', clone._handleMessage);
     this.socket = null;
     return clone;
   }
 
-  listen() {
-    if (this.mode !== MODE_NONE) throw new Error('Invalid socket mode. Cannot change a client socket to server');
-    if (!this.onConnection) throw new Error('Listening sockets must provide onConnection handler');
-    this.mode = MODE_SERVER;
-    this.clients = {};
-  }
-
-  connect(address: AddressInfo, payload: any) {
-    if (this.mode !== MODE_NONE) throw new Error('Invalid socket mode. Cannot change a server socket to a client');
-    this.mode = MODE_CLIENT;
-    this.remoteAddress = address;
-    this.privateKey = randomKey();
-    this.onTerminate = () => {
-      this.socket.close();
-    }
-
-    const publicKey = powmod(DIFFE_BASE, this.privateKey, DIFFE_MODULO);
-    const buf = this._prepareConnect([publicKey, Date.now(), payload]);
-    this._send(buf, 0, buf.length, address.port, address.address);
-  }
-
-  broadcast(msg: any, address?: string) {
-    const buf = this._prepareBcast(msg);
-    this._send(buf, 0, buf.length, this.port, address);
-  }
-
-  private handleData(data: Uint8Array) {
-    const remoteSeq = data[1];
-    const remoteAck = data[2];
-    const chunk = data[3];
-
-    let trySend = false;
-    // Use the acknowledgement
-    if (remoteAck === this.seq % 256) {
-      // Remove the last send data from the queue
-      this._clearTransmit();
-      trySend = this.txQueue.length > 0;
-    }
-
-    if (remoteSeq === (this.ack + 1) % 256) {
-      // Accept data in sequence
-      this.ack = remoteSeq;
-      // Looks like we got something to process
-      if (!this.rxBuffer) {
-        this.rxBuffer = data;
-      } else {
-        // Append data to the rxBuffer
-        const newBuf = new Uint8Array(this.rxBuffer.length + data.length - 4);
-        newBuf.set(data, newBuf.length - data.length);
-        newBuf.set(this.rxBuffer);
-        this.rxBuffer = newBuf;
-      }
-
-      // Keep the data in the rx buffer
-      if (chunk === 0) {
-        const payload = this._parseData(this.rxBuffer);
-        this.rxBuffer = null;
-        this.onMessage(payload);
-      }
-
-      trySend = true;
-    }
-
-    // Send an ack with a packet if available
-    if (trySend) this._trySend();
-  }
-
-  private _parseData(buf: Uint8Array): any {
-    buf.set(HEADER);
-    const msg = JSON.parse(uint8ToStr(buf));
-    return msg[1];
-  }
-
-  send(payload: any) {
-    if (this.mode !== MODE_CLIENT) throw new Error('Socket is not a client and cannot send messages');
-    // Create chunks and queue them
-    // Add extra space for headers `["",` 4 bytes
-    const msg = strToUint8(JSON.stringify(["", payload]));
-    const trySend = this.txQueue.length === 0 && !this.txPending;
-
-    // Divide it into chunks of little less than 1000 bytes
-    let chunks = Math.floor((msg.length - 1) / CHUNK_SIZE) + 1;
-    if (chunks > 256) throw new Error(`Payload can at max be ${CHUNK_SIZE * 256} bytes. Your payload is ${msg.length} bytes`);
-    let offset = 4; // Start offset with the header
-    do {
-      chunks -= 1;
-      this.txQueue.push([chunks, offset, msg]);
-      offset += CHUNK_SIZE;
-    } while (chunks > 0);
-
-    if (trySend) this._trySend();
-    // this._send(, payload, this.remoteAddress.port, this.remoteAddress.address);
-  }
-
-  private _clearTransmit() {
-    clearTimeout(this._txTimer);
-    this.txPending = null;
-  }
-
-  private _unitTransmit = () => {
-    this._try += 1;
-    if (this._try > this.retries) {
-      // number of retries exceeded, we couldn't send something
-      this.close();
-      return;
-    }
-
-    const [chunk, offset, msg] = this.txPending;
-    msg[offset - 4] = DATA;
-    msg[offset - 3] = this.seq;
-    msg[offset - 2] = this.ack;
-    msg[offset - 1] = chunk;
-
-    this._send(msg, offset - 4, Math.min(msg.length - offset, CHUNK_SIZE) + 4, this.remoteAddress.port, this.remoteAddress.address);
-
-    // retry almost immediately
-    this._txTimer = setTimeout(this._unitTransmit, 300);
-  }
-
-  private _trySend() {
-    if (this.txQueue.length === 0 || this.txPending) {
-      const emptyAck = new Uint8Array(4);
-      emptyAck[0] = DATA;
-      emptyAck[1] = this.seq % 256;
-      emptyAck[2] = this.ack % 256;
-      emptyAck[3] = 0;
-      this._send(emptyAck, 0, 4, this.remoteAddress.port, this.remoteAddress.address);
-    } else if (!this.txPending) {
-      this._try = 0;
-      this.txPending = this.txQueue.shift();
-      this.seq = (this.seq + 1) % 256;
-      this._unitTransmit();
+  send(data: any) {
+    if (this.state !== STATE_CONNECTED) {
+      console.warn('Socket is not connected. Data will not be sent');
+    } else {
+      this.tx.add(MSG_MESSAGE, data);
+      this.tx.try();
     }
   }
 
-  private _prepareConnect(payload: ConnectionPayload) {
-    const msg = JSON.stringify(payload);
-    const buf = strToUint8(msg);
-    // Replace the starting '[' with the connect code
-    buf[0] = CONNECT;
-    return buf;
-  }
+  private _send(type: number, port: number, ip: string, buf: Uint8Array, offset: number = 0, length: number = buf.length, chunk: number = 0, isEmpty: boolean = false) {
+    buf[0] = type;
+    buf[1] = this.version;
+    buf[2] = isEmpty ? this.seq : incr(this.seq);
+    buf[3] = this.ack;
+    buf[4] = chunk;
 
-  private _parseConnect(buf: Uint8Array): ConnectionPayload {
-    buf[0] = HEADER[0];
-    return JSON.parse(uint8ToStr(buf));
-  }
-
-  private _prepareBcast(payload: any) {
-    const msg = JSON.stringify([payload]);
-    const buf = strToUint8(msg);
-    // Replace the starting space with bcast code
-    buf[0] = BCAST;
-    return buf;
-  }
-
-  private _parseBcast(buf: Uint8Array) {
-    buf[0] = HEADER[0];
-    return JSON.parse(uint8ToStr(buf))[0];
-  }
-
-  private _send(buf: Uint8Array, offset: number, length: number, port: number, ip: string = '255.255.255.255') {
-    // xor with the shared key
-    // this._parse(buf);
     this.socket.send(buf, offset, length, port, ip);
   }
 }
---
+++ src/utils.ts
@@ -0,0 +1,109 @@
+import { strToUint8, uint8ToStr } from './strToUint8';
+
+// Maximum chunksize (limited by android devices)
+const CHUNK_SIZE = 960;
+
+export function createPayload(data: any) {
+  return strToUint8(JSON.stringify([" ", data]));
+}
+const HEADER = strToUint8('[" ",');
+const BLANK = strToUint8(JSON.stringify([' ', null]));
+
+export function parsePayload(payload: Uint8Array) {
+  payload.set(HEADER);
+  return JSON.parse(uint8ToStr(payload))[1];
+}
+
+export class RxBuffer {
+  private buffer: Uint8Array;
+
+  append(buffer: Uint8Array): any {
+    const chunk = buffer[4];
+    let buf = buffer;
+    if (this.buffer) {
+      buf = new Uint8Array(this.buffer.length + buffer.length - HEADER.length);
+      buf.set(buffer, buf.length - HEADER.length);
+      buf.set(this.buffer);
+      this.buffer = undefined;
+    } else {
+      buf.set(HEADER);
+    }
+
+    if (chunk === 0) return JSON.parse(uint8ToStr(buf))[1];
+    this.buffer = buf;
+  }
+
+  clear() {
+    this.buffer = undefined;
+  }
+}
+
+export class TxQueue {
+  private queue: Array<[number, Uint8Array, number, number, number, boolean]> = [];
+  private handle: ReturnType<typeof setTimeout> = null;
+  tries: number = 0;
+  private readonly sender: () => void;
+
+  constructor(sender: () => void) {
+    this.sender = sender;
+  }
+
+  pop() {
+    if (!this.handle) {
+      return null;
+    }
+
+    clearTimeout(this.handle);
+    this.tries = 0;
+    this.handle = null;
+    return this.queue.shift()[0];
+  }
+
+  peek() {
+    return this.queue[0];
+  }
+
+  add(type: number, data: any) {
+    const payload = createPayload(data);
+    // Divide paylaod into multiple chunks if needed
+    let chunks = Math.floor((payload.length - 1) / CHUNK_SIZE) + 1;
+    if (chunks > 256) throw new Error(`Payload can be at max be ${CHUNK_SIZE * 256} bytes. Your payload is ${payload.length} bytes including headers`);
+    let offset = 0; // Start offset with the header
+    do {
+      chunks -= 1;
+      this.queue.push([
+        type,
+        payload,
+        chunks,
+        offset,
+        chunks === 0 ? (payload.length - offset) : (CHUNK_SIZE + HEADER.length),
+        data === null,
+      ]);
+      offset += CHUNK_SIZE;
+    } while (chunks > 0);
+  }
+
+  activate(type: number) {
+    if (!this.handle && this.queue.length === 0) {
+      this.queue.push([type, BLANK, 0, 0, BLANK.length, true]);
+    }
+  }
+
+  try() {
+    if (this.handle || this.queue.length === 0) return;
+
+    // Setup a timer for retry
+    this.handle = setTimeout(this.sender, 300);
+
+    // Try to send
+    this.sender();
+  }
+
+  abort() {
+    if (this.handle) {
+      clearTimeout(this.handle);
+      this.handle = null;
+      this.queue = null;
+    }
+  }
+}
